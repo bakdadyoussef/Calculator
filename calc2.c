61df7395e48f9581f24b9ce864d1943fcfe36dd6
bakdadyoussef <bakdadyoussef@gmail.com>
2026-08-31 10:47:57 +0100

    Add initial calculator class with Qt includes

---
+++ calc2.c
@@ -0,0 +1,10 @@
+#include Qwidget
+#include Qapplication
+#include Qpushbutton
+#include Qgridlayout
+#include Qedit line
+
+class calculator : public Qwidget {
+Q_object
+public:
+calculator(Qwidget *parent=nullptr)  : Qwidget(parent) {
